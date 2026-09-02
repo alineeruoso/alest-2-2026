2987a967ec642829bc89bbfd537de6baa156c58f
Aline Ruoso <aline.godoi@edu.pucrs.br>
2026-09-01 23:06:02 -0300

    add main classes

---
+++ App.java
@@ -0,0 +1,6 @@
+public class App {
+    //loop - O programa inicia, 
+    // imprime um prompt (sugestão: Pennywise>) e espera comandos.
+
+    //scanner - switch case para os comandos
+}
---
+++ Heap.java
@@ -0,0 +1,7 @@
+
+
+public class Heap {
+    
+    //métodos atualiza, 
+}
+// precisa fazer a classe Priority Queue ou da pra usar a do java??
